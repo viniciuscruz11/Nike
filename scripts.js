192a1a99e951db2d738540308a678a611e05081d
viniciuscruz11 <viniciusfcruz@outlook.com>
2025-05-25 22:49:08 -0300

    Alterações nos efeitos dos botões

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 let body = document.querySelector("body")
 let tenis = document.querySelector(".imagem-tenis")
+let botao = document.querySelector(".botao-carrinho")
 // document = html
 // querySelector = pegador / selecionador
 
@@ -8,7 +9,8 @@ function mudarVisual(cor, imagem){
     
     body.style.background = cor
     
-
+    botao.style.background = cor
+    
     // contador de tempo
     setTimeout(() => {
         tenis.src = imagem
